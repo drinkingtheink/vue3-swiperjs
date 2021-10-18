b2b89a8cf972cda14455045bd1b38f3adbb53fa6
drinkingtheink <jason.m.harrison@gmail.com>
2021-10-18 12:13:28 -0500

    Inital project commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AccountCarousel/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AccountCarousel from './components/AccountCarousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AccountCarousel
   }
 }
 </script>
